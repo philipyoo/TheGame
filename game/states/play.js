105bf0aab8d584a989a3eebb1b1a0b2eab3d36bb
Philip Yoo <philipyoo10@gmail.com>
2015-09-19 16:12:20 -0700

    Update prefab constructors, commented out ground for now, add second player for testing purposes

--- game/states/play.js
+++ game/states/play.js
@@ -16,39 +16,31 @@
 
       this.background = this.game.add.sprite(0, 0, 'background');
 
-      this.player = new Player(this.game, 100, 100);
-      this.game.add.existing(this.player);
 
-      this.ground = new Ground(this.game, 0, 700, 2000, 112);
-      this.game.add.existing(this.ground);
+      this.player1 = new Player(this.game, 100, 100, 'player1', true);
+      this.game.add.existing(this.player1);
 
-      this.game.camera.follow(this.player);
+
+      //movement for these are the same because of same keystrokes
+      this.player2 = new Player(this.game, 200, 100, 'player2', false);
+      this.game.add.existing(this.player2);
+
+      // this.ground = new Ground(this.game, 0, 700, 2000, 112);
+      // this.game.add.existing(this.ground);
+
+      this.game.camera.follow(this.player1);
 
       // cursors = this.game.input.keyboard.createCursorKeys();
 
     },
     update: function() {
-      console.log('yolo again');
-      console.log(this);
+      
+      this.game.physics.enable(this.player1);
 
-      this.game.physics.enable(this.player);
-
-      this.game.physics.arcade.collide(this.player, this.ground);
-
-      // if (cursors.up.isDown) {
-      //   console.log(this);
-      //   this.game.camera.y -= 4;
-      // } else if (cursors.down.isDown) {
-      //   this.game.camera.y += 4;
-      // }
-      //
-      // if (cursors.left.isDown) {
-      //   this.game.camera.x -= 4;
-      // } else if (cursors.right.isDown) {
-      //   this.game.camera.x += 4;
-      // }
+      this.game.physics.arcade.collide(this.player1, this.ground);
 
     },
+
     clickListener: function() {
       this.game.state.start('gameover');
     }
